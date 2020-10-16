b6192872abe9cf4896caf072653ad766a5e27f92
katylurdht <katyhuamanitito1@gmail.com>
2020-10-16 16:23:38 -0500

    agrgando archivo mdlink

--- src/index.js
+++ src/index.js
@@ -2,157 +2,167 @@ const path = require('path'); //  modulo para trabajar con routes de archivos y 
 const fs = require('fs'); // fs sistema de modulo(archivo)
 const marked = require('marked');
 const index = require ('./index.js');
+const fetch = require('node-fetch');
+const { isAbsolute } = require('path');
 
-// funcion que verifica si es una route absoluta
-const IsAbsolute = (route) => path.isAbsolute(route);
 
-// funcion que transforma a route absoluta
-const ResolveRoute = (route) => path.resolve(route);
+//Funcio que verifica si existe la route
+const existsPath = (route) => (fs.existsSync(route));
 
-// funcion que verifica si es un archivo
-const IsFile = (route) => fs.statSync(route).isFile();
+//funcion que transforma una route relativa absoluta
+const Isabsolute =(route) => (path.isAbsolute(route));
 
-// funcion que permite leer un directorio
-const ReadDirectory = (route) => fs.readdirSync(route);
+//funcion que verifica si es archivo
+const IsFile = ((route) => fs.statSync(route).isFile());
 
-// funcion que verifica si es un directorio
-const IsDirectory = (route) => fs.lstatSync(route).isDirectory(); // ojo revisar lstatSync: es para prueba
+// console.log('hola katy');
+// console.log(IsFile('C:\\Users\\KELLY-PC\\Documents\\md-links\\LIM013-fe-md-links\\src\\Prueba2\\archivo2.md'));
 
-// funcion que verifica  si es un archivo markdow
-//método devuelve la extensión del path .md
-const IsFilemd = (route) => path.extname(route);
+// const IsDirectory= (route) => fs.readdirSync(route).isDirectory();
 
-//--------------------------------------- funcion lee el contenido de una ruta de directorio--------------------------------
+//Funcion que extrae si tiene extencion .md
+const IsMd = (route) => (path.extname(route));
 
-function listcontent(route) { // lee el contenido del directorio y devuelve lista de contenido(prueba1.md prueba2.md)
-  return fs.readdirSync(route);
-}
-// console.log(listcontent('C:\\Users\\KELLY-PC\\Documents\\md-links\\LIM013-fe-md-links\\Prueba2'));
+// funcion que lee directorio
+const readDirectorio = (ruta)=>fs.readdirSync(ruta);
 
-// ------------------------------Funcion que convierte una ruta relativa a una route absoluta------------------------------
-const convertRouteAbsolute = ((route) => {
-  if (!IsAbsolute(route)) { // si isabsolute es distinto a route absoluta
-    return ResolveRoute(route);// ruta relativa transformado a absoluta
+
+
+//-------------------------------------------Funcion que lee directorio--------------------------------------------------------------//
+
+const getArrayOfFilesAndDirectories = (route) => {
+  return readDirectorio(route).map(element =>//se crea una nueva matriz con los elementos encontrados
+    path.join(route, element),); // unimos los elementos en un array y lo separamos por ","
+  };
+
+//console.log(getArrayOfFilesAndDirectories('C:\\Users\\KELLY-PC\\Documents\\md-links\\LIM013-fe-md-links\\src\\Prueba2')) ruta
+
+/* retorna [
+  'C:\\Users\\KELLY-PC\\Documents\\md-links\\LIM013-fe-md-links\\src\\Prueba2\\archivo.md', //Ruta prueba2  //elemto archivo.md
+  'C:\\Users\\KELLY-PC\\Documents\\md-links\\LIM013-fe-md-links\\src\\Prueba2\\archivo2.md',
+  'C:\\Users\\KELLY-PC\\Documents\\md-links\\LIM013-fe-md-links\\src\\Prueba2\\nuevaCarpeta'
+]
+*/
+
+//--------------------------------------------FUNCIÓN para convertir route relativa a absoluta-----------------------------------//
+const convertAbsolute = ((route) => {
+  if(!Isabsolute(route)) { // si Isabsolute es distinto a la ruta
+    const newAbsolute = path.resolve(route); // transforma ruta relativa a absoluta
+    return newAbsolute;
   }
-  return route; // muestre la route inicial (absoluta)
-});
-console.log( convertRouteAbsolute('src\Prueba2')); //transforma a relativa
-
-// ****************************************funcion principal********************************************************************************
-const metodoprincipal = ((route) => {
-let arraymdFiles = [];// aqui se almacenara archivos md
-  const routeAbsoluta = convertRouteAbsolute(route); //ruta absoluta sera igual a funcion convertRouteAbsolute(transformacion de ruta)
-  console.log ("Nueva route "+ routeAbsoluta);
-  console.log("****");
- console.log(listcontent(routeAbsoluta));
-  let arrayresult = [];// se almacenaran objetos
-  arrayresult=listcontent(routeAbsoluta);//liscontent trae toda los objeto de la route(archivo.md,nueva carpeta)y lo trae en un array
-  arrayresult.forEach(element=>{ //recore cada elemento(1x1)
-    if(IsFilemd(element)==='.md') { // por cada elemento preguntamos si tiene extencion .md
-      arraymdFiles.push(element);//arrayfiles contendra elemento con .md y con push agregamos el elemento al array
-      console.log('tiene extencion md');
-    }
-  });
-  arraymdFiles.forEach(element=>{ //recorrer cada elemento files.md
-    console.log(element);
-  })
-
-  // 2) es un file ?
-    if(IsFile(routeAbsoluta)){ // si la route pasada no devuelve un archivo
-      console.log("es un file****");
-    }else{
-      console.log("No es un file")
-      return false;
-    }
-    if(IsFilemd(routeAbsoluta)===".md"){
-      arraymdFiles.push(routeAbsoluta);
-      console.log ("archivo .md");
-      return true
-    }else{
-      console.log("El archivo no tiene extension .md")
-
-    }
+  return route; //retorna ruta inicial(seria absoluta)
 });
 
-// console.log('inicio_Katy');
-// console.log(metodoprincipal('C:\\Users\\KELLY-PC\\Documents\\md-links\\LIM013-fe-md-links\\src\\Prueba2\\archivo2.md'));
-// console.log("metodo nuevo "+ metodoprincipal('C:\\Users\\KELLY-PC\\Documents\\md-links\\LIM013-fe-md-links\\Prueba\\prueba 2.md'));
-// console.log("metodo nuevo "+ metodoprincipal('Prueba2'));
-// console.log('fin_katy');
+// console.log(convertAbsolute('Prueba2'));
 
-
-
-////************************ANTES **************************** */
-// funcion que verifica si es directorio
-// const encontrarFile = (route) => {
-//   if (!IsFile(route)) { // si isfile es distinto a la route
-//     // return IsDirectory(route); // entonces verificar si es directorio
-//     return LeeDirectorio(route); // lee directorio
+// const traerarcchivosmd = (ruta)=>{
+//   let arraymd =[]
+//   const path = convertAbsolute(ruta);
+//   if(IsFile(ruta)){
+//     if(Ismd(path)==='.md');{
+//     arraymd.push(arraymd);
+//     }
 //   }
-//   return route;
-// };
-
-// const readingDirectories = (route) => fs.readdirSync(route);
 
 
-//------------------------------------------------------------------------------------------//
 
-const getMdFiless = (route)=>{
-  let arrayresult = [];
-  let arraymdFiles =[];
-  arrayresult=listcontent(route);//Funcion liscontent trae todos los objeto de la route(archivo.md,nueva carpeta)y lo traen un array
-  // console.log('hola');
-  // console.log(arrayresult);
-  arrayresult.forEach(element=>{ //recorer elelemento
-    if(IsFilemd(element)==='.md') { // por cada elemento preguntamos si tene exten .md
-      arraymdFiles.push(element);//arrayfiles contendra elemento con .md push agregar elemento al array
-      // console.log('tiene extencion md')
+// }
+//------------------------------------------------FUNCIÓN que trae archivos .md--------------------------------------------------------//
+
+// se pasa ruta console.log(searchPathFiles('Prueba2'));
+
+const searchPathFiles = (route) => {
+  let arrayMdFiles = []; // array almacenara archivos con extención .md
+  const filePath = convertAbsolute(route); // asignamos ruta absoluta a filepath
+  if (IsFile(filePath)) { // pregunta si es un archivo
+    if (IsMd(filePath) === '.md') { //por cada elemento preguntamos si tiene extencion .md y la extrae
+      arrayMdFiles.push(filePath);   //arrayfiles contendra elemento con .md y con push agregamos el elemento al array
     }
-  }); // en este array se iran agregando archivos
-return arraymdFiles;
-};
-// console.log('inicio');
-// console.log(getMdFiless('C:\\Users\\KELLY-PC\\Documents\\md-links\\LIM013-fe-md-links\\src\\Prueba2'));
-// console.log('fin');
-
-// -----------------------------------------Funcion para extraer links---------------------------------------------------//
-const extraerLinks= (route) => {
-  if (!IsAbsolute(route)) {
   } else {
-    const newRouteAb = ResolveRoute(route);
-    let lastArrayLinks = []; //últimos enlaces de matriz
-    const renderer = new marked.Renderer();
-    console.log('hello');
-    newRouteAb.forEach((file) => {
-      renderer.link = (href, title, text) => {
-        const InfoLink = {
-          href:href,
-          text:text,
-          file:file,
-        };
-        lastArrayLinks.push(InfoLink);
-      };
-      marked(ReadDirectory(file), { renderer });
+    getArrayOfFilesAndDirectories(route).forEach((element) => {// recorido por cada elemento de directorio
+      const filesOfNewRoute = element;
+      //obtener archivos MD en nueva ruta
+      const getMDFilesInNewRoute = searchPathFiles(filesOfNewRoute);
+      arrayMdFiles = arrayMdFiles.concat(getMDFilesInNewRoute); //concat() se usa para unir dos o más arrays arrayMdFiles, getMDFilesInNewRoute
     });
-    return lastArrayLinks;
   }
+  return arrayMdFiles;
 };
 
-  console.log('inicializa');
-  console.log(extraerLinks('C:\\Users\\KELLY-PC\\Documents\\md-links\\LIM013-fe-md-links\\Prueba2\\archivo2.md'));
-  console.log('finaliza');
+
+// se envia ruta de directorio
+
+/*retorna solo archivos .md [
+  'C:\\Users\\KELLY-PC\\Desktop\\katy-LIM013-fe-md-links\\src\\Prueba2\\archivo.md',
+  'C:\\Users\\KELLY-PC\\Desktop\\katy-LIM013-fe-md-links\\src\\Prueba2\\archivo2.md'
+]
+*/
+//------------------------------------------------Funcion que trae archivo---------------------------------------------------------//
+
+//se envia console.log(readFilePath('C:\\Users\\KELLY-PC\\Desktop\\katy-LIM013-fe-md-links\\src\\Prueba2\\archivo2.md'));
+// resultado trae todo la informacion del archivo
+
+const readFilePath = (route) => fs.readFileSync(route).toString();
+
+//-----------------------------------------Funcion que nos permite extraer links de archivos--------------------------------------------
+
+// se envia console.log(extraerLinks('C:\\Users\\KELLY-PC\\Desktop\\katy-LIM013-fe-md-links\\src\\Prueba2\\archivo2.md'));
+/*
+  {
+    href: 'https://developer.mozilla.org/es/docs/Glossary/Callback_function',
+    text: 'MDN',
+    file: 'C:\\Users\\KELLY-PC\\Desktop\\katy-LIM013-fe-md-links\\src\\Prueba2\\archivo2.md'
+  }
+*/
+const extraerLinks = (route) => {
+      let arrayLinks = [];// aqui se ira agregando links
+      const renderer = new marked.Renderer();
+      searchPathFiles(route).forEach((file) => {    //ejecuta acción por cada elemento
+        renderer.link= (href, title, text) => { // renderer salida ouput con tres propiedades
+          const propLink = {
+            href, //url encontrada
+            text, //texto que aprece dentro de link
+            file //ruta de archivo deonde se encuentra link
+          };
+          arrayLinks.push(propLink); //con push se va agregando a arraylinks
+        };
+        marked(readFilePath(file), { renderer });
+      });
+      return arrayLinks; // devuelve links de carpeta dentro de src
+    };
+
+    // console.log(extraerLinks('C:\\Users\\KELLY-PC\\Desktop\\katy-LIM013-fe-md-links\\src\\Prueba2\\archivo2.md'));
+
+
+
+// ejemplo
+// function readFilePromise(fileName)
+// {
+//     var promise = new Promise(function(resolve, reject) {
+//         fs.readFile(fileName, function(err, buffer) {
+//             if (err) {
+//                 reject(err);
+//                 return;
+//             }
+//             resolve(buffer);
+//         });
+//     });
+
+//     return promise;
+// }
+
+// var promise = readFilePromise("file.txt");
+// promise.then(
+//     function(buffer) {
+//         // console.log("FILE CONTENTS", buffer.toString());
+//     },
+//     function(err) {
+//         // console.log("FILE READ ERROR", err);
+//     }
+// );
+// console.log(addValidate('C:\\Users\\KELLY-PC\\Desktop\\katy-LIM013-fe-md-links\\src\\Prueba2\\archivo2.md'));
 
 module.exports = {
-  IsAbsolute,
-  ResolveRoute,
-  IsFile,
-  ReadDirectory,
-  IsDirectory,
-  IsFilemd,
-  listcontent,
-  convertRouteAbsolute,
-  metodoprincipal,
-  getMdFiless,
+  existsPath,
   extraerLinks,
-
 };
--- src/mdlink.js
+++ src/mdlink.js
@@ -1,1 +1,33 @@
-const { IsAbsolute } = require('./index');
+const funciones = require('./index.js'); // (./) index esta dentro de la misma carpeta
+
+// console.log(index);
+
+const mdlinks = (stringpath,opcion) =>{ // creamos mdliks 2 parametros
+    if (!funciones.existsPath(stringpath)) { // 
+        throw new Error('Ruta invalida');
+    }else{
+        let links= funciones.extraerLinks(stringpath);
+        //    console.log(links);
+        new Promise(
+            (resolve, reject)  => {
+                resolve(links);
+            }
+        )
+    }
+};
+
+mdlinks('C:\\Users\\KELLY-PC\\Documents\\md-links\\LIM013-fe-md-links\\src\\Prueba2');
+/*
+[
+    {
+      href: 'https://www.geeksforgeeks.org/node-js-fs-statsync-method/',
+      text: 'metodo',
+      file: 'C:\\Users\\KELLY-PC\\Documents\\md-links\\LIM013-fe-md-links\\src\\Prueba2\\archivo.md'
+    },
+    {
+      href: 'https://developer.mozilla.org/es/docs/Glossary/Callback_function',
+      text: 'MDN',
+      file: 'C:\\Users\\KELLY-PC\\Documents\\md-links\\LIM013-fe-md-links\\src\\Prueba2\\archivo2.md'
+    }
+  ]
+  */
